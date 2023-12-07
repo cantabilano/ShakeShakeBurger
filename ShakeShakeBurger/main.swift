cd50b6a6abb0bcf9672cbc4fe34dd08c0634f95b
Jason Yang <didwnsgus@gmail.com>
2023-12-07 11:00:54 +0900

    잘 되나요?

--- ShakeShakeBurger/main.swift
+++ ShakeShakeBurger/main.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-// MARK: 주메뉴 구현 class 구현되나요~~~~~~~~~~ 해봅시다.
+// MARK: 주메뉴 구현 class 구현되나요~~~~~~~~~~ 해봅시다. 잘 되나요?
 class Menu {
     func printMainMenu() {
         while true {
