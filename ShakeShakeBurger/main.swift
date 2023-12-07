94b91bfd9182d3b1b47d586125a7019e7aca8979
Jason Yang <didwnsgus@gmail.com>
2023-12-07 21:39:29 +0900

    class WishList 추가

--- ShakeShakeBurger/main.swift
+++ ShakeShakeBurger/main.swift
@@ -9,10 +9,14 @@ import Foundation
 
 
 // MARK: 주메뉴 구현 class(클래스 명은 첫글자 대문자)
-class Menu {
 
+class Menu {
     func printMainMenu() {
         while true {
+            print("""
+                "SHAKESHACK BURGER 에 오신걸 환영합니다."
+                 아래 메뉴판을 보시고 메뉴를 골라 입력해주세요.
+""")
             print("[ SHAKESHACK MENU ]")
             print("1. Burgers         | 앵거스 비프 통살을 다져만든 버거")
             print("2. Frozen Custard  | 매장에서 신선하게 만드는 아이스크림")
@@ -61,14 +65,24 @@ class Menu {
                     break
                 } else if input == 1 {
                     print("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거")
+                    let wishList = WishList()
+                    wishList.choiceMenu()
                 } else if input == 2 {
                     print("2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거")
+                    let wishList = WishList()
+                    wishList.choiceMenu()
                 } else if input == 3 {
                     print("3. Shroom Burger | W 9.4 | 몬스터 치즈와 체다 치즈로 속을 채운 베지테리안 버거")
+                    let wishList = WishList()
+                    wishList.choiceMenu()
                 } else if input == 4 {
                     print("4. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거")
+                    let wishList = WishList()
+                    wishList.choiceMenu()
                 } else if input == 5 {
                     print("5. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거")
+                    let wishList = WishList()
+                    wishList.choiceMenu()
                 } else {
                     print("선택한 메뉴: \(input), 메뉴에 없습니다. 다른 메뉴를 골라주세요")
                 }
@@ -95,14 +109,24 @@ class Menu {
                     return
                 case 1 :
                     print("1.  FrozenCustard1   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거")
+                    let wishList = WishList()
+                    wishList.choiceMenu()
                 case 2 :
                     print("2.  FrozenCustard2   | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거")
+                    let wishList = WishList()
+                    wishList.choiceMenu()
                 case 3 :
                     print("3.  FrozenCustard3   | W 9.4 | 몬스터 치즈와 체다 치즈로 속을 채운 베지테리안 버거")
+                    let wishList = WishList()
+                    wishList.choiceMenu()
                 case 4 :
                     print("4.  FrozenCustard4   | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거")
+                    let wishList = WishList()
+                    wishList.choiceMenu()
                 case 5 :
                     print("5.  FrozenCustard5   | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거")
+                    let wishList = WishList()
+                    wishList.choiceMenu()
                 default :
                     print("메뉴번호를 콘솔창에 적어주세요.")
                 }
@@ -117,14 +141,12 @@ class Menu {
         
 // MARK: 메뉴 클래스 객체화(인스턴스)
 let menu = Menu()
-
 // MARK: menu 객체에 printMainMenu() 메소드 호출 = 콘솔창에 프린터
 menu.printMainMenu()
 
 
 // MARK: Beers 클래스 구현
 class Beers: Menu {
-
     func printBeerMenu() {
         while true {
             print("[ Beers MENU ]")
@@ -141,12 +163,20 @@ class Beers: Menu {
                     return
                 case 1:
                     print("1. Craft Beer 1   | W 6.9 | 뉴욕 브루클린 브루어리에서 양조한 첫 번째 맥주")
+                    let wishList = WishList()
+                    wishList.choiceMenu()
                 case 2:
                     print("2. Craft Beer 2   | W 8.9 | 뉴욕 브루클린 브루어리에서 양조한 두 번째 맥주")
+                    let wishList = WishList()
+                    wishList.choiceMenu()
                 case 3:
                     print("3. Local Beer 1   | W 9.4 | 지역 양조장에서 생산한 첫 번째 맥주")
+                    let wishList = WishList()
+                    wishList.choiceMenu()
                 case 4:
                     print("4. Local Beer 2   | W 6.9 | 지역 양조장에서 생산한 두 번째 맥주")
+                    let wishList = WishList()
+                    wishList.choiceMenu()
                 default:
                     print("메뉴번호를 콘솔창에 적어주세요.")
                 }
@@ -156,11 +186,8 @@ class Beers: Menu {
         }
     }
 }
-
-
 // MARK: Beers 클래스 객체화
 let beerMenu = Beers()
-
 // MARK: beerMenu 객체에 printBeerMenu() 메소드 호출
 beerMenu.printBeerMenu()
 
@@ -182,12 +209,20 @@ class Drinks : Menu {
                     return
                 case 1:
                     print("1. Cocacola   | W 6.9 | 미국 콜라")
+                    let wishList = WishList()
+                    wishList.choiceMenu()
                 case 2:
                     print("2. Sprite   | W 8.9 | 미쿡 사이다")
+                    let wishList = WishList()
+                    wishList.choiceMenu()
                 case 3:
                     print("3. 환타   | W 9.4 | 코카콜라의 탄산음료 마싯따")
+                    let wishList = WishList()
+                    wishList.choiceMenu()
                 case 4:
                     print("4. 물   | W 6.9 | 물이 제일 좋아")
+                    let wishList = WishList()
+                    wishList.choiceMenu()
                 default:
                     print("메뉴번호를 콘솔창에 적어주세요.")
                 }
@@ -199,5 +234,35 @@ class Drinks : Menu {
 }
 
 let drinkMenu = Drinks()
-
 drinkMenu.printDrinksMenu()
+
+
+class WishList {
+    
+    func choiceMenu() {
+        while true {
+            print("위 메뉴를 장바구니에 추가하시겠습니까?")
+            print("1. 확인    2. 취소")
+            print("선택한 메뉴 : ")
+            if let input = Int(readLine() ?? "") {
+                switch input {
+                case 0 :
+                    print("메인 메뉴로 이동합니다.")
+                    return
+                case 1 :
+                    print("()가 장바구니에 추가되었습니다.")
+                    return
+                case 2 :
+                    print("취소")
+                    return
+                default :
+                    print("장바구니 추가 여부를 확인해주세요.")
+                }
+            } else {
+                print("error: 숫자를 적어주세요.")
+            }
+        }
+    }
+}
+let wishList = WishList()
+wishList.choiceMenu()
