a11be1e2326dcdc81f68662331066a879a0c4f7b
Jason Yang <didwnsgus@gmail.com>
2023-12-07 10:25:36 +0900

    1. //MARK: 주석으로 메뉴 구분 2. 보조메뉴1 구현 함수 구현

--- ShakeShakeBurger/main.swift
+++ ShakeShakeBurger/main.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-// 주메뉴 구현 class
+// MARK: 주메뉴 구현 class
 class Menu {
     func printMainMenu() {
         while true {
@@ -37,15 +37,15 @@ class Menu {
             }
         }
     }
-    // 보조메뉴1 구현 함수 혹은 class로 구현 필요
+    // MARK: 보조메뉴1 함수 구현(12. 7. 10:00)
     func printBurgerMenu() {
         while true {
             print("[ Burgers MENU ]")
             print("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거")
             print("2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거")
             print("3. Shroom Burger | W 9.4 | 몬스터 치즈와 체다 치즈로 속을 채운 베지테리안 버거")
-            print("3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거")
-            print("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거")
+            print("4. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거")
+            print("5. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거")
             print("0. 뒤로가기      | 뒤로가기")
             
             if let input = Int(readLine() ?? "") {
@@ -54,15 +54,23 @@ class Menu {
                     break
                 } else if input == 1 {
                     print("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거")
+                } else if input == 2 {
+                    print("2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거")
+                } else if input == 3 {
+                    print("3. Shroom Burger | W 9.4 | 몬스터 치즈와 체다 치즈로 속을 채운 베지테리안 버거")
+                } else if input == 4 {
+                    print("4. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거")
+                } else if input == 5 {
+                    print("5. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거")
                 } else {
-                    print("선택한 메뉴: \(input)")
+                    print("선택한 메뉴: \(input), 메뉴에 없습니다. 다른 메뉴를 골라주세요")
                 }
             } else {
                 print("error: 숫자를 적어주세요.")
             }
         }
     }
-    // 보조메뉴2 구현 함수 혹은 class로 구현 필요
+    // MARK: 보조메뉴2 구현 함수 혹은 class로 구현 필요
      func printFrozenCustardMenu() {
          while true {
              print("[ FrozenCustard MENU ]")
@@ -87,8 +95,8 @@ class Menu {
      }
 }
 
-//메뉴 클래스 객체화(인스턴스)
+// MARK: 메뉴 클래스 객체화(인스턴스)
 let menu = Menu()
 
-//menu 객체에 printMainMenu() 메소드 호출 = 콘솔창에 프린터
+// MARK: menu 객체에 printMainMenu() 메소드 호출 = 콘솔창에 프린터
 menu.printMainMenu()
